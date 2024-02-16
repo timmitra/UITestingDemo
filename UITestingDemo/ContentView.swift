c57e59c916ab3a6c559f343035cd2e98ea411181
Tim Mitra <tim@it-guy.com>
2024-02-16 02:21:10 -0500

    Initial Commit

---
+++ UITestingDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UITestingDemo
+//
+//  Created by Tim Mitra on 2024-02-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UITestingDemo/UITestingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestingDemoApp.swift
+//  UITestingDemo
+//
+//  Created by Tim Mitra on 2024-02-16.
+//
+
+import SwiftUI
+
+@main
+struct UITestingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
