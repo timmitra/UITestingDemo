80b3be97a95ef83ff0034e7fa0dd48fe601b6a5d
Tim Mitra <tim@it-guy.com>
2024-02-17 12:33:34 -0500

    add user prefs

--- UITestingDemo/ContentView.swift
+++ UITestingDemo/ContentView.swift
@@ -27,6 +27,42 @@ struct ContentView: View {
       }, label: {
         Text(!user.isLoggedIn ? "Login" : "Logout")
       }).accessibilityIdentifier("loginButton")
+      
+      if user.isLoggedIn {
+        Form {
+          Section {
+            VStack {
+              Text("Color theme")
+              Picker("", selection: $user.colorTheme) {
+                Text("Light").tag(0)
+                Text("Dark").tag(1)
+              }
+              .pickerStyle(SegmentedPickerStyle())
+              .accessibilityIdentifier("colorTheme")
+            }
+          }
+          Section {
+            HStack {
+              Text("Text Size")
+              Slider(value: $user.textSize, in: 1...100)
+                .accessibilityIdentifier("slider")
+                }
+            }
+          Section {
+            VStack {
+              Text("Font")
+              Picker("", selection: $user.font) {
+                Text("Arial").tag("Arial")
+                Text("Avenir Next").tag("Avenir Next")
+                Text("Noteworthy").tag("Noteworthy")
+                Text("Futura").tag("Futura")
+              }
+              .pickerStyle(WheelPickerStyle())
+              .accessibilityIdentifier("fontPicker")
+            }
+          }
+          }
+        }
     }
     .sheet(isPresented: $showLogin) {
             LoginView()
--- UITestingDemo/User.swift
+++ UITestingDemo/User.swift
@@ -12,6 +12,9 @@ class User: ObservableObject {
   @Published var isLoggedIn = false
   @Published var username = ""
   @Published var password = ""
+  @Published var colorTheme = 0
+  @Published var textSize: Double = 14
+  @Published var font = "Arial"
   
   func login() -> Bool {
     guard username == "test" && password == "pass" else {
